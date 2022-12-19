8b0d3c1fffe0ea0e22106680016ed16aa4440d77
Benjamin Grohbiel <ben.grohbiel@snyk.io>
2022-12-19 13:57:45 +0000

    Improve conditional type example

--- conditional-types.ts
+++ conditional-types.ts
@@ -1,4 +1,13 @@
-type DogAge = Dog extends Animal ? number : string;
+// function declaration with conditional return type
+function double<T extends number | string>(
+  x: T
+): T extends string ? string : number;
 
-const aDog: DogAge = 1;
-// const anotherDog: DogAge = "doggie";
+// followed by function implementation, following immediately
+function double(x: any) {
+  return x + x;
+}
+
+const aNumber = double(10);
+const aString = double("10");
+// const aObject = double({}); NOT OK, not string | number
